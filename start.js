75d44d5b6b2ce87a26ea9674bfeca4e3065d25a5
Tobias Liebig <work@etobi.de>
2017-06-18 22:12:17 +0200

    ramp

--- start.js
+++ start.js
@@ -109,6 +109,7 @@ main = function () {
 					if (message.toString() == 'OFF') {
 						value = 'off';
 					}
+					value = value + ';ramp:1';
 					logger.info('> domiq', ' ', address, ' = ', value);
 					domiqClient.write(address, value);
 
